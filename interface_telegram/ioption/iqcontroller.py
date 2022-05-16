68d2f8034e17536206a38caeed29e7d4c7d6709a
Natanniel <natanniel.alves@outlook.com>
2022-05-16 16:56:14 -0300

    deploy

--- interface_telegram/ioption/iqcontroller.py
+++ interface_telegram/ioption/iqcontroller.py
@@ -1,5 +1,5 @@
 from dados import retornaTodosDadosDoUsuario
-from interface_telegram.ioption.iqoptionapi.stable_api import IQ_Option
+#from interface_telegram.ioption.iqoptionapi.stable_api import IQ_Option
 
 def verificarSaldo(chat_id):
     saldo = 0
@@ -10,7 +10,7 @@ def verificarSaldo(chat_id):
     for row in gerenciamento:
         print('Tem algo em gerenciamento')
 
-        API = IQ_Option(row[5], row[6])
+      #  API = IQ_Option(row[5], row[6])
 
 
     return saldo,iqoptionConfigurado
